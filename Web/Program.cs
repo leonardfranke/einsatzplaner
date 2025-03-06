726eb9bfd2e336aa25dc0b56c000f89b5a68c61b
Leonard Franke <leonard.franke@t-online.de>
2025-03-06 19:00:31 +0100

    Added Dockerfile for web

--- Web/Program.cs
+++ Web/Program.cs
@@ -14,7 +14,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://192.168.178.48:5000/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IDepartmentService, DepartmentService>();
 builder.Services.AddScoped<IBackendManager, BackendManager>();
 builder.Services.AddScoped<AuthManager>();
