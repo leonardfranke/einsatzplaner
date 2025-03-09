4d5fa4b0ddf0f4b1e3ce5c7647b279290154d394
Leonard Franke <leonard.franke@t-online.de>
2025-03-09 17:22:18 +0100

    Adds Nginx, Adds Env Variable

--- Web/Program.cs
+++ Web/Program.cs
@@ -14,7 +14,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var apiBaseUrl = Environment.GetEnvironmentVariable("API_BASE_URL");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 builder.Services.AddScoped<IDepartmentService, DepartmentService>();
 builder.Services.AddScoped<IBackendManager, BackendManager>();
 builder.Services.AddScoped<AuthManager>();
