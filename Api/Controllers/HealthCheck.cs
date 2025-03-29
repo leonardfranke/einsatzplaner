db1ad3a65a89e5b414c62074b04134dc0b54fad1
Leonard Franke <leonard.franke@t-online.de>
2025-03-29 14:57:13 +0100

    Fügt HealthCheck hinzu

---
+++ Api/Controllers/HealthCheck.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheck : Controller
+    {
+        [HttpGet]
+        public Task Get()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
