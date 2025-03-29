96c3880e0c4b1fd8c2b8ff70ab8f30d6b140ad0e
Leonard Franke <leonard.franke@t-online.de>
2025-03-29 14:27:50 +0100

    Setzt optimierte Helper

--- Optimizer/Loader.py
+++ Optimizer/Loader.py
@@ -7,24 +7,30 @@ app = firebase_admin.initialize_app()
 db = firestore_async.client()
 
 async def fetch_events():
-	eventsRef = db.collection("Department").document("v87avboSu7Dc74ZJpJFk").collection("Event").list_documents()
+	eventsRef = db.collection("Department").document("v87avboSu7Dc74ZJpJFk").collection("Event")
+
 	events = []
-	async for eventRef in eventsRef:
+	async for eventRef in eventsRef.list_documents():
 		helpers = await eventRef.collection("Helper").get()
 		event = Event([])
 		for helperRef in helpers:
 			helperSnapshot = helperRef.to_dict()
 			if helperRef.read_time < helperSnapshot["LockingTime"]:
 				lockedMembers = []
-				availableMembers = helperSnapshot["SetMembers"].append(helperSnapshot["SetMembers"])
+				availableMembers = helperSnapshot["SetMembers"].append(helperSnapshot["QueuedMembers"])
 			else:
 				lockedMembers = helperSnapshot["SetMembers"]
-				availableMembers = helperSnapshot["SetMembers"]
+				availableMembers = helperSnapshot["QueuedMembers"]
 
 			event.Helpers.append(Helper(helperRef.id, eventRef.id, helperSnapshot["HelperCategoryId"], helperSnapshot["RequiredAmount"], lockedMembers, availableMembers))
 		events.append(event)
 	
 	filledHelpers = Optimize(events)
-	print(filledHelpers)
+	
+	for filledHelper in filledHelpers:
+		await eventsRef.document(filledHelper.EventId).collection("Helper").document(filledHelper.Id).update({
+			"SetMembers": filledHelper.SetMembers,
+			"QueuedMembers": filledHelper.RemainingMembers,
+		})
 
 asyncio.run(fetch_events())
