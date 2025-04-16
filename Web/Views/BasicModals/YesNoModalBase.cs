aa3700781f645e2eabec05e7b7475406324aad94
leonard <leonard.franke@t-online.de>
2025-04-16 12:22:10 +0200

    Ergänzt generisches YesNoModal

---
+++ Web/Views/BasicModals/YesNoModalBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Web.Views
+{
+    public class YesNoModalBase : ComponentBase
+    {
+        [Parameter]
+        public string Text { get; set; }
+
+        [Parameter]
+        public string TrueButtonText { get; set; }
+
+        [Parameter]
+        public string FalseButtonText { get; set; }
+
+        [Parameter]
+        public Func<bool, Task> ResultFunc { get; set; }
+
+        [Parameter]
+        public Func<Task> CloseModalFunc { get; set; }
+
+        public async Task SendResult(bool result)
+        {
+            await ResultFunc(result);
+            await CloseModalFunc();
+        }
+    }
+}
