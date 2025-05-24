0d6c64d0e058ba96ba63c39a3feb389fa1930235
leonard <leonard.franke@t-online.de>
2025-05-24 12:48:19 +0200

    Fixt Übergeben des API Keys

--- Api/Manager/User/UserManager.cs
+++ Api/Manager/User/UserManager.cs
@@ -62,7 +62,6 @@ namespace Api.Manager
             try
             {
                 var uri = Path.Combine(_httpClient.BaseAddress.ToString(), SendVerificationMailRequest.URL + _apiKey);
-                Console.WriteLine($"Verification mail request send to: {uri} with {requestBody}");
                 var httpResponse = await _httpClient.PostAsJsonAsync(uri, requestBody);                
             }
             catch
