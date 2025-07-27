bc263773ca36a83f63853afe893218b4d31aa57e
leonard <leonard.franke@t-online.de>
2025-07-27 20:49:18 +0200

    Catch entfernt, Zeitzoneninfo an Linux angepasst

--- Api/Manager/Tasks/TaskManager.cs
+++ Api/Manager/Tasks/TaskManager.cs
@@ -15,43 +15,35 @@ namespace Api.Manager
 
         public Task TriggerRecalculation(string departmentId, DateTime? dateTime = null)
         {
-            try
-            {
-                if (_optimizerEndPoint == null)
-                    return Task.CompletedTask;
-
-                if (dateTime == null)
-                {
-                    var berlinTimeZone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
-                    dateTime = TimeZoneInfo.ConvertTime(DateTime.Today.AddDays(1), berlinTimeZone);
-                }
-                else if (dateTime.Value.Kind != DateTimeKind.Local)
-                    throw new ArgumentException("dateTime", "DateTime kind must be local");
-
-                var httpRequest = new GTask.HttpRequest()
-                {
-                    Url = $"{_optimizerEndPoint}?departmentId={departmentId}",
-                    HttpMethod = GTask.HttpMethod.Get
-                };
-
-                var queueName = new GTask.QueueName("einsatzplaner", "europe-west1", "Optimization");
-                var taskId = $"{departmentId}-{dateTime.Value.ToString("yyyy-MM-dd")}";
-                var taskHash = Math.Abs(taskId.GetHashCode()).ToString();
-                var taskName = new GTask.TaskName(queueName.ProjectId, queueName.LocationId, queueName.QueueId, taskHash);
-                var googleTask = new GTask.Task()
-                {
-                    HttpRequest = httpRequest,
-                    TaskName = taskName
-                };
-                var secondsSinceEpoch = new DateTimeOffset(dateTime.Value.ToUniversalTime()).ToUnixTimeSeconds();
-                googleTask.ScheduleTime = new Google.Protobuf.WellKnownTypes.Timestamp { Seconds = secondsSinceEpoch };
-                return _client.CreateTaskAsync(queueName, googleTask);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
+            if (_optimizerEndPoint == null)
                 return Task.CompletedTask;
+
+            if (dateTime == null)
+            {
+                var berlinTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Europe/Berlin");
+                dateTime = TimeZoneInfo.ConvertTime(DateTime.Today.AddDays(1), berlinTimeZone);
             }
+            else if (dateTime.Value.Kind != DateTimeKind.Local)
+                throw new ArgumentException("dateTime", "DateTime kind must be local");
+
+            var httpRequest = new GTask.HttpRequest()
+            {
+                Url = $"{_optimizerEndPoint}?departmentId={departmentId}",
+                HttpMethod = GTask.HttpMethod.Get
+            };
+
+            var queueName = new GTask.QueueName("einsatzplaner", "europe-west1", "Optimization");
+            var taskId = $"{departmentId}-{dateTime.Value.ToString("yyyy-MM-dd")}";
+            var taskHash = Math.Abs(taskId.GetHashCode()).ToString();
+            var taskName = new GTask.TaskName(queueName.ProjectId, queueName.LocationId, queueName.QueueId, taskHash);
+            var googleTask = new GTask.Task()
+            {
+                HttpRequest = httpRequest,
+                TaskName = taskName
+            };
+            var secondsSinceEpoch = new DateTimeOffset(dateTime.Value.ToUniversalTime()).ToUnixTimeSeconds();
+            googleTask.ScheduleTime = new Google.Protobuf.WellKnownTypes.Timestamp { Seconds = secondsSinceEpoch };
+            return _client.CreateTaskAsync(queueName, googleTask);
         }
     }
 }
