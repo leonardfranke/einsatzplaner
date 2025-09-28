594c0d9f70308b2f9524e57a0e4272e9549da793
leonard <leonard.franke@t-online.de>
2025-09-28 21:42:43 +0200

    Gibt Unbekannt aus, falls Nutzer nicht gefunden wird

--- Web/Extensions/HelperExtensions.cs
+++ Web/Extensions/HelperExtensions.cs
@@ -13,13 +13,14 @@ namespace Web.Extensions
 
         public static MarkupString GetRegisteredMembers(this Models.Helper helper, IEnumerable<Member> members)
         {
+            var unknownUserString = "Unbekannter Nutzer";
             var str = "";
             if (helper.LockedMembers.Any())
-                str += $"Fest: {string.Join(", ", helper.LockedMembers.Select(memberId => members.First(member => member.Id == memberId)).Select(member => member.Name))}<br/>";
+                str += $"Fest: {string.Join(", ", helper.LockedMembers.Select(memberId => members.FirstOrDefault(member => member.Id == memberId)).Select(member => member?.Name ?? unknownUserString))}<br/>";
             if (helper.PreselectedMembers.Any())
-                str += $"Ausgewählt: {string.Join(", ", helper.PreselectedMembers.Select(memberId => members.First(member => member.Id == memberId)).Select(member => member.Name))}<br/>";
+                str += $"Ausgewählt: {string.Join(", ", helper.PreselectedMembers.Select(memberId => members.FirstOrDefault(member => member.Id == memberId)).Select(member => member?.Name ?? unknownUserString))}<br/>";
             if (helper.AvailableMembers.Any())
-                str += $"Verfügbar: {string.Join(", ", helper.AvailableMembers.Select(memberId => members.First(member => member.Id == memberId)).Select(member => member.Name))}<br/>";
+                str += $"Verfügbar: {string.Join(", ", helper.AvailableMembers.Select(memberId => members.FirstOrDefault(member => member.Id == memberId)).Select(member => member?.Name ?? unknownUserString))}<br/>";
             return new MarkupString(str);
         }
 
