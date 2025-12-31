7b87634ed633e6097e05af324b19dd51d7bb520a
leonard <leonard.franke@t-online.de>
2025-12-31 17:53:34 +0100

    Erlaubt Eintragung unabhängig von Gruppe

--- Web/Views/Checkbox/HelperCheckboxBase.cs
+++ Web/Views/Checkbox/HelperCheckboxBase.cs
@@ -8,8 +8,6 @@ namespace Web.Views
 {
     public class HelperCheckboxBase : ComponentBase
     {
-        private IEnumerable<Models.Group> _requiredGroups;
-
         [CascadingParameter]
         public Modal Modal { get; set; }
 
@@ -28,9 +26,6 @@ namespace Web.Views
         [Parameter]
         public Models.Role Role { private get; set; }
 
-        [Parameter]
-        public IEnumerable<Models.Group> Groups { private get; set; }
-
         public bool IsPreselectedOrAvailable => IsPreselected || IsAvailable;
 
         public bool IsFill => Helper.FillMembers.Contains(currentUserId);
@@ -39,9 +34,7 @@ namespace Web.Views
 
         public bool IsEventInFuture => Event.EventDate.ToUniversalTime() >= DateTime.UtcNow;
 
-        public bool IsMemberPermitted => Role != null && _requiredGroups != null 
-            && (_requiredGroups.Count() == 0 || _requiredGroups.SelectMany(group => group.MemberIds).Contains(Member.Id)) 
-            && (Role.MemberIds.Contains(Member.Id) || Role.IsFree);
+        public bool IsMemberPermitted => Role != null && (Role.MemberIds.Contains(Member.Id) || Role.IsFree);
 
         public bool IsPlayerLocked => Helper.LockedMembers.Contains(currentUserId);
 
@@ -83,11 +76,6 @@ namespace Web.Views
         protected override async Task OnInitializedAsync()
         {
             currentUserId = (await _authManager.GetLocalUser()).Id;
-        }
-
-        protected override void OnParametersSet()
-        {
-            _requiredGroups = Groups.Where(group => Helper.RequiredGroups.Contains(group.Id));
         }
 
         protected Task<bool> SetIsAvailable(bool setHelping)
