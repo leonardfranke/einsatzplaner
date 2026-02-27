e5eef2e8488d478f262fe45a72eb8ad59efd7a31
leonard <leonard.franke@t-online.de>
2026-02-27 18:08:54 +0100

    Fixt Migrationfile

--- supabase/migrations/20260227161941_firebase-columns-to-text-type.sql
+++ supabase/migrations/20260227161941_firebase-columns-to-text-type.sql
@@ -1,17 +1,33 @@
-drop index if exists "api"."Departments_pkey";
-
-drop index if exists "api"."Members_pkey";
-
-alter table "api"."Departments" alter column "Id" set data type text using "Id"::text;
-
-alter table "api"."Members" alter column "DepartmentId" set data type text using "DepartmentId"::text;
-
-alter table "api"."Members" alter column "Id" set default ''::text;
-
-alter table "api"."Members" alter column "Id" set data type text using "Id"::text;
-
-CREATE UNIQUE INDEX "Departments_pkey" ON api."Departments" USING btree ("Id");
-
-CREATE UNIQUE INDEX "Members_pkey" ON api."Members" USING btree ("Id");
+ALTER TABLE api."Members"
+DROP CONSTRAINT IF EXISTS "Members_DepartmentId_fkey";
 
 
+ALTER TABLE api."Members"
+DROP CONSTRAINT IF EXISTS "Members_pkey";
+
+ALTER TABLE api."Departments"
+DROP CONSTRAINT IF EXISTS "Departments_pkey";
+
+
+ALTER TABLE api."Departments"
+ALTER COLUMN "Id" SET DATA TYPE text USING "Id"::text;
+
+ALTER TABLE api."Members"
+ALTER COLUMN "Id" SET DATA TYPE text USING "Id"::text;
+
+ALTER TABLE api."Members"
+ALTER COLUMN "DepartmentId" SET DATA TYPE text USING "DepartmentId"::text;
+
+
+ALTER TABLE api."Departments"
+ADD CONSTRAINT "Departments_pkey" PRIMARY KEY ("Id");
+
+ALTER TABLE api."Members"
+ADD CONSTRAINT "Members_pkey" PRIMARY KEY ("Id");
+
+
+ALTER TABLE api."Members"
+ADD CONSTRAINT "Members_DepartmentId_fkey"
+FOREIGN KEY ("DepartmentId")
+REFERENCES api."Departments"("Id")
+ON DELETE CASCADE;
