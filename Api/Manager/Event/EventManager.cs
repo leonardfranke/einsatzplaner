b69d84f1719ccdc306d66df79a84fecad9d4a03d
leonard <leonard.franke@t-online.de>
2026-09-25 10:27:29 +0200

    remove available entering from calendar

--- Api/Manager/Event/EventManager.cs
+++ Api/Manager/Event/EventManager.cs
@@ -193,7 +193,7 @@ namespace Api.Manager
             var requirements = GetRequirements(departmentId, null, null);
             await foreach (var requirement in requirements)
             {
-                if(requirement.LockedMembers.Contains(memberId) || requirement.PreselectedMembers.Contains(memberId) || requirement.AvailableMembers.Contains(memberId))
+                if(requirement.LockedMembers.Contains(memberId) || requirement.PreselectedMembers.Contains(memberId))
                     yield return requirement;
             }
         }
